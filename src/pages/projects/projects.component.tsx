3a6cb2b6c0914fd156bf0a78058166c13dc92a44
anasbloor <79023313+anasbloor@users.noreply.github.com>
2022-03-18 18:56:52 -0400

    Update projects.component.tsx

--- src/pages/projects/projects.component.tsx
+++ src/pages/projects/projects.component.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
                 <Card border="primary" style={{ width: 'inherit' }}>
                     <Card.Header>Automated water dispenser</Card.Header>
                     <Card.Body>
-                    <Card.Title></Card.Title>
+                    <Card.Title>LabVIEW, procurement</Card.Title>
                     <Card.Text>
                         Programmed using LabVIEW and incorporated redundant loops for user safety.
                         (https://www.youtube.com/watch?v=3aziS4YVB7g)
@@ -23,10 +23,9 @@ const Projects = () => {
                 <Card border="secondary" style={{ width: 'inherit' }}>
                     <Card.Header>Formula SAE</Card.Header>
                     <Card.Body>
-                    <Card.Title>Secondary Card Title</Card.Title>
+                    <Card.Title>Machining, Welding, CAD, Compliance</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk
-                        of the card's content.
+                        Converted a broken motorcycle into a formula SAE car. Designed and built the frame in compliance with Formula SAE guidebook.
                     </Card.Text>
                     </Card.Body>
                 </Card>
@@ -35,10 +34,9 @@ const Projects = () => {
                 <Card border="success" style={{ width: 'inherit' }}>
                     <Card.Header>Human powered vehicle</Card.Header>
                     <Card.Body>
-                    <Card.Title>Success Card Title</Card.Title>
+                    <Card.Title>Machining, Welding, CAD</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk
-                        of the card's content.
+                        Built a recumbent bike with a welded aluminum frame and a carbon fiber cover using molding techniques in a machine shop.
                     </Card.Text>
                     </Card.Body>
                 </Card>
@@ -47,10 +45,9 @@ const Projects = () => {
                 <Card border="primary" style={{ width: 'inherit' }}>
                     <Card.Header>Rocket stabilization system</Card.Header>
                     <Card.Body>
-                    <Card.Title>Primary Card Title</Card.Title>
+                    <Card.Title>Fusion, MATLab</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk
-                        of the card's content.
+                        Built a stabilization system for York University's rocketry club to control the trajectory of the flight, access the height attained, and perform landing maneuvre.
                     </Card.Text>
                     </Card.Body>
                 </Card>
@@ -59,22 +56,21 @@ const Projects = () => {
                 <Card border="secondary" style={{ width: 'inherit' }}>
                     <Card.Header>FLUX news</Card.Header>
                     <Card.Body>
-                    <Card.Title>Secondary Card Title</Card.Title>
+                    <Card.Title>Public speaking, Brand growth</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk
-                        of the card's content.
+                        Presented research taking place at Yor University's engineering department. Simplying complex technical concepts into digestible and informative videos.
                     </Card.Text>
                     </Card.Body>
                 </Card>
                 <br />
 
                 <Card border="success" style={{ width: 'inherit' }}>
-                    <Card.Header>Mechanical energy storage</Card.Header>
+                    <Card.Header>Atlas Bike Repair</Card.Header>
                     <Card.Body>
-                    <Card.Title>Success Card Title</Card.Title>
+                    <Card.Title>Client facing communication</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk
-                        of the card's content.
+                        Founded a fast-growing mobile bike repair service company with over 300 satisfied customers and established partnerships.
+                        (https://www.youtube.com/watch?v=b0fMat7Mqy0)
                     </Card.Text>
                     </Card.Body>
                 </Card>
