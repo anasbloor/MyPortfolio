b6769705286f7a375120cfa0ae76373f874096f2
anasbloor <79023313+anasbloor@users.noreply.github.com>
2022-04-13 02:00:44 -0400

    url button test

--- src/pages/about/about.component.tsx
+++ src/pages/about/about.component.tsx
@@ -55,7 +55,7 @@ const About = () => {
                                 </div>
                                 <div>
                                     <a href="#contact">
-                                    <button><a href='https://google.com' alt='Broken Link'>This is a button</a></button>
+                                    <button><a href='https://google.com'>This is a button</a></button>
                                     </a>
                                 </div>
                             </Row> 
